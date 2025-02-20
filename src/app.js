c91d488ffd08cac02b23b6723337277525360af4
Majuyi <majuyitan@gmail.com>
2025-02-20 17:41:43 +0100

    Add app.js

---
+++ src/app.js
@@ -0,0 +1,25 @@
+// ./src/app.js
+
+const express = require('express');
+const cors = require('cors');
+const cron = require('node-cron');
+const { getRandomPoem } = require('./utils/getRandomPoem');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use(express.static('public'));
+app.use('/', require('./routes/index'));
+app.use('/api', require('./routes/integration'));
+app.use('/tick', require('./routes/tick'));
+
+// Cron job for local testing
+cron.schedule('0 8 * * *', () => {
+    console.log('Sending poem of the day...');
+    console.log(getRandomPoem());
+});
+
+module.exports = app;
